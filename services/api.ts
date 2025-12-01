c3d9fd17a2058b04c675e088ededa432fb877ade
EsraYamanCode <yamaann.esra@gmail.com>
2025-12-01 22:42:01 +0300

    initial commit

---
+++ services/api.ts
@@ -0,0 +1,39 @@
+export const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`,
+    }
+
+}
+export const fetchMovies = async ({query}: {query: string}) => {
+
+   // console.log("API KEY OKUNUYOR MU:", process.env.EXPO_PUBLIC_API_KEY);
+
+    const endpoint = query
+        ?`${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        :`${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    //console.log("ENDPOINT:", endpoint);
+    //console.log("HEADERS:", TMDB_CONFIG.headers);
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok) {
+        //@ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+
+    const data = await response.json();
+    return data.results;
+}
+
+/* /discover/movie */
+/* api okuma erişim jetonu;
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzQ1MDgzYWZkMjZjNzRiMjU3MGI5ZmI3ODY2NzZlNyIsIm5iZiI6MTc2MzY0MDEwNS4yNjQsInN1YiI6IjY5MWYwMzI5NWNiZWM1YmNjMmMyY2U5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LkPeFdK1HO3PiR26sbusahVfF5sf-k2kyzYJwMaz5GU */
+/* api anahtarı;
+c345083afd26c74b2570b9fb786676e7 */
---
+++ services/appwrite.ts
@@ -0,0 +1,52 @@
+//track the searches made by a user
+
+import {Client, Databases, ID, Query} from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const TABLE_ID= process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID!;
+
+console.log(DATABASE_ID, TABLE_ID);
+
+const client = new Client()
+client
+    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    .setProject("691838ad001bde65a2d2")
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
+            Query.equal('searchTerm', query)
+        ])
+        if (result.documents.length > 0) {
+            const existingMovie = result.documents[0];
+
+            // @ts-ignore
+            await database.updateDocument(
+                DATABASE_ID,
+                TABLE_ID,
+                existingMovie.$id,
+                {
+                    count: existingMovie.count + 1
+                }
+            )
+        } else {
+            await database.createDocument(DATABASE_ID, TABLE_ID, ID.unique(), {
+                searchTerm: query,
+                movie_id: movie.id,
+                count: 1,
+                title: movie.title,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    //check if a record of that search has already been stored
+    //if a document is found increment the searchCount field
+    //if no document is found c
+        //create a new document in Appwrite database -> 1
+}
+
